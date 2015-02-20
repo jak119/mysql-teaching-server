0f40808ea928aca0a51d468f842d32f6448d0e1d
abagheri <abagheri@wireless1x-155-41-85-229.bu.edu>
2015-02-20 16:20:12 -0500

    More edits

--- web/IS_script.php
+++ web/IS_script.php
@@ -1,5 +1,5 @@
 <?php 
     $user=$_POST["login_name"];
     $user_pw=$_POST["df_pass"];
-    $output=exec("/usr/bin/createdb $user $user $user_pw") 
+    $output=exec("/usr/bin/createdb $user $user $user_pw"); 
 ?>
