2e76a37bef40938bf79dea1e358cbf02c54e5bf2
abagheri <abagheri@wireless1x-155-41-85-229.bu.edu>
2015-02-20 17:11:50 -0500

    Big changes

--- web/class_script.php
+++ web/class_script.php
@@ -1,13 +1,8 @@
 <?php 
-    $target_dir = "/home/uploads/";
+    $target_dir = "/tmp/uploads/";
     $target_file = $target_dir . basename($_FILES["class_list"]["name"]);
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
     // Check file size
     if ($_FILES["class_list"]["size"] > 500000) {
         echo "Sorry, your file is too large.";
@@ -29,8 +24,4 @@
             echo "Sorry, there was an error uploading your file.";
         }
     }
-
-    $class_list=$_POST["class_list"];
-    $user_pw=$_POST["df_pass2"];
-    $output=exec("/usr/bin/add_class $class_list $user_pw");
 ?>
