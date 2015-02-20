cbcbda2f34fe79cb12cd69c9c443c8fff4853ea4
abagheri <abagheri@wireless1x-155-41-85-229.bu.edu>
2015-02-20 16:24:07 -0500

    Revert

--- web/class_script.php
+++ web/class_script.php
@@ -1,13 +1,5 @@
 <?php 
     $class_list=$_POST["class_list"];
     $user_pw=$_POST["df_pass2"];
-        
-    $handle = fopen("$class_list", "r");
-    if ($handle) {
-        while (($line = fgets($handle)) !== false) {
-            $output=exec("/usr/bin/createdb $line $line $user_pw");
-        }
-        fclose($handle);
-    } 
-}
+    $output=exec("/usr/bin/add_class $class_list $user_pw");
 ?>
