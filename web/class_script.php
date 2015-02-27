3ad17775c312f4378a4b4daebe357d187ef19139
abagheri <abagher@users.noreply.github.com>
2015-02-27 16:14:02 -0500

    Update class_script.php

--- web/class_script.php
+++ web/class_script.php
@@ -1,13 +1,8 @@
-<?php 
-    $target_dir = "/home/uploads/";
+<?php
+    $target_dir = "/tmp/uploads/";
     $target_file = $target_dir . basename($_FILES["class_list"]["name"]);
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
     // Check file size
     if ($_FILES["class_list"]["size"] > 500000) {
         echo "Sorry, your file is too large.";
@@ -24,12 +19,12 @@
     // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["class_list"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["class_list"]["name"]). " has been uploaded and the users have been added to the database.";
+            echo "The file ". basename( $_FILES["class_list"]["name"]). " has been uploaded.";
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
     }
 
-    $user_pw=$_POST["df_pass2"];
-    $output=exec("/usr/bin/addclass $class_list $user_pw");
+        $user_pw=$_POST["df_pass2"];
+        $output=exec("/usr/bin/addclass $target_file $user_pw");
 ?>
